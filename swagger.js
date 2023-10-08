227d01bea61608a5d2f542af35386865caab4202
Lindsay Garner <gar21034@byui.edu>
2023-10-07 18:22:09 -0600

    https

--- swagger.js
+++ swagger.js
@@ -13,7 +13,7 @@ const outputFile = 'swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
 
 /* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
+  'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
